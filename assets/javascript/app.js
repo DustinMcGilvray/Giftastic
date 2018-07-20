f4c5790a821f551596308d057405d25d39aed38e
DustinMcGilvray <15370964+DustinMcGilvray@users.noreply.github.com>
2018-07-19 22:08:15 -0500

    Added Style

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,48 +1,64 @@
-// $(document).ready(function () {
+$(document).ready(function () {
   //Array of Topics
   var topics = ["80s Movies", "80s Toys", "80s Music", "80s Fashion"];
-
+  var click=0;
   //Function for Displaying Gifs
   function displayEighties() {
     $("#topics").empty();
     var eightiesInfo = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + eightiesInfo + "&api_key=sm8z5Yxja8rinARsGr6GRGuwebdVVY9o&limit=10";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + eightiesInfo + "&api_key=sm8z5Yxja8rinARsGr6GRGuwebdVVY9o&offset=20&limit=10";
 
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function (response) {
+      $("#topicsCard").show();
       for (var i = 0; i < response.data.length; i++) {
         var imageDiv = $("<div class='eightiesInfo'>");
-        var imgUrlplay = response.data[i].images.fixed_height_small.url;
-        var imgUrlPaused = response.data[i].images.fixed_height_small_still.url;
+        var imgUrlPlay = response.data[i].images.fixed_height.url;
+        var imgUrlPaused = response.data[i].images.fixed_height_still.url;
         
         var rating = response.data[i].rating;
         var r = $("<p>").text("Rating: " + rating);
-        var downloadButton = $("<button>").text("Download");
-        var d = $("<a>").attr("src", imgUrl.download);
+        var downloadButton = $("<a download>");
+        var d = downloadButton.attr("href",response.data[i].images.fixed_height.url);
+        var button = $("<button id='downloadButton'>").text("Download");
+        d.append(button);
+        // console.log(images.fixed_height_small.url);
 
-        var image = $("<img>").attr("src", imgUrlPaused);
-        // image.attr("data-pause", imgUrlPaused);
-        // image.attr("data-play", imgUrlplay);
-        // image.attr("data-status", "status");
-        // image.addClass("picture");
+        //Holds the images, download, and ratings.
+        var imageHolder = $("<div class= 'imageHolder'>");
 
-        $("#topics").append(image);
-        $("#topics").append(r);
-        $("#topics").append(downloadButton);
+        var image = $("<img>");
+        image.attr("src", imgUrlPaused);
+        image.attr("data-pause", imgUrlPaused);
+        image.attr("data-play", imgUrlPlay);
+        image.attr("data-status", "stop");
+        image.addClass("picture");
+        image.addClass("card")
+
+        imageHolder.append(image);
+        imageHolder.append(r);
+        imageHolder.append(button);
+
+
+        $("#topics").append(imageHolder);
       }
     });
   }
 
+  
   //Function for dumping the JSON content for each button into the div
   function topicButtons() {
     $("#buttons").empty();
+    $("#topicsCard").hide();
     for (var i = 0; i < topics.length; i++) {
-      var b = $("<button>");
-      b.addClass("80sButton");
+      var b = $("<button class='btn'>");
+      b.addClass("eightiesButton");
       b.attr("data-name", topics[i]);
       b.text(topics[i]);
+      if (b.attr("data-click"))
+      b.attr("data-click")
       $("#buttons").append(b);
     }
   }
@@ -50,37 +66,22 @@
   //Function for Playing and Pausing Gifs
   function pausePlayGifs() {
     var status = $(this).attr("data-status");
-    if (status === "data-status") {
-      $(this).attr("src", imageUrlPaused);
-      $(this).attr("data-pause", "pause")
-    } else if (status === "imageUrl") {
-      $(this).attr("src", imgUrlplay);
-      $(this).attr("data-play", "play")
+    var pause = $(this).attr("data-pause");
+    var play = $(this).attr("data-play");
+    if (status === "stop") {
+      $(this).attr("src", play);
+      $(this).attr("data-status", "animate")
+    } else if (status === "animate") {
+      $(this).attr("src", pause);
+      $(this).attr("data-status", "stop")
     }
   }
-
-  // //Function for dumping the JSON URL content for each button into the div
-  // function downloadGif() {
-  //   $("#download").empty();
-  //   for (var i = 0; i < topics.length; i++) {
-  //     var d = $("<button>");
-  //     d.addClass("downloadButton");
-  //     d.attr("data-name", topics[i]);
-  //     d.html("Download");
-  //     $("#download").append(d);
-  //   }
-  // }
-
-  // $("#download").on("click", function (event) {}
-  //     event.preventDefault();
-  //     var gifDownload = $(response.data[i].images.fixed_height_small.url;);
-
 
   //Function Call 
   topicButtons();
 
   //On Click For Creating Buttons for New Search Requests
-  $("#addButton").on("click", function (event) {
+  $("#searchButton").on("click", function (event) {
     event.preventDefault();
     var eighties = $("#topic_input").val().trim();
     topics.push(eighties);
@@ -88,10 +89,10 @@
   });
 
   //On-Click Event for Play/Pause of Gifs
-  // $(document).on("click", ".picture", pausePlayGifs);
+  $(document).on("click", ".picture", pausePlayGifs);
 
   //On Click Event to all Buttons
-  $(document).on("click", ".80sButton", displayEighties);
+  $(document).on("click", ".eightiesButton", displayEighties);
 
 
-// });
+});
